6c39afa4ec3a09545055313f3e504695554b338a
Alex Noyanov <alex.noyanov@gmail.com>
2016-10-13 21:59:20 +0300

    Changed some functions in Car library. Now all functions can input working time values. Comments added

--- Car/Car.cpp
+++ Car/Car.cpp
@@ -11,48 +11,50 @@
 int spd = 100;
 
 //  Motor pins:
-int lfwd =3;
-int lbk = 5;
-int rfwd = 4;
-int rbk = 6;
+int lfwd = 3;   // Left motor moves forward
+int lbk  = 5;   // Left motor moves back
+int rfwd = 4;   // Right motor moves forward
+int rbk  = 6;   // Right motor moves back
 
-//  Main Functions: fwd stp bck lft rgt
-void STP(){
+//  Main Functions:
+
+void STP(int tme){          // Stop
     digitalWrite(rbk,HIGH);
     digitalWrite(lbk,HIGH);
     digitalWrite(rfwd,HIGH);
     digitalWrite(lfwd,HIGH);
+    delay(tme);
 }
 
-void FWD(){
+void FWD(int tme){          // Forward
     digitalWrite(rfwd,LOW);
     digitalWrite(lfwd,LOW);
     delay(spd);
     digitalWrite(rfwd,HIGH);
     digitalWrite(lfwd,HIGH);
-    //delay(spd);
+    delay(tme);
 }
-void BCK(){
+void BCK(int tme){          // Back
     digitalWrite(rbk,LOW);
     digitalWrite(lbk,LOW);
     delay(spd);
     digitalWrite(rbk,HIGH);
     digitalWrite(lbk,HIGH);
-    //delay(spd);
+    delay(tme);
 }
-void LFT(){
+void LFT(int tme){          // Left
     digitalWrite(lbk,LOW);
     digitalWrite(rfwd,LOW);
     delay(spd);
     digitalWrite(lbk,HIGH);
     digitalWrite(rfwd,HIGH);
-    //delay(spd);
+    delay(tme);
 }
-void RGT(){
+void RGT(int tme){          // Right
     digitalWrite(rbk,LOW);
     digitalWrite(lfwd,LOW);
     delay(spd);
     digitalWrite(rbk,HIGH);
     digitalWrite(lfwd,HIGH);
-    //delay(spd); 
+    delay(int tme);
 }
