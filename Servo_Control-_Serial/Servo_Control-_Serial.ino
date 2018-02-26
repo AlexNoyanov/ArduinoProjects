ca99d0bb46881c518bd595052e3fd126a5c83cc2
AlexNoyanov <alex.noyanov@gmail.com>
2018-02-26 09:54:28 +0300

    Servo control from serial port using Processing program. This project will use OpenCV to detect faces and move camera

---
+++ Servo_Control-_Serial/Servo_Control-_Serial.ino
@@ -0,0 +1,29 @@
+// Arduino Servo Camera moving
+
+// Project with OpenCV face detecting 
+// Camera following faces
+
+// Created by Alex Noyanov 
+// The 26th of February 
+
+#include <Servo.h> // Library for servo
+
+Servo servoX;    // Servo X 
+
+
+int angleX;     // X angle
+void setup() {
+  // put your setup code here, to run once:
+  servoX.attach(8); // Connetcing servo to pin 8
+  Serial.begin(9600); // Start serial port on 9600 bod
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  angleX = Serial.read();   // Read X angle
+
+  servoX.write(angleX);     // Rotate servo on angle
+  delay(500);               // Wait a litle bit
+}
