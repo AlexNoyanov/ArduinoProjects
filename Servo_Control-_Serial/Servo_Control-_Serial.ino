1d8b99ea0d11fd3ff4d180757e70d97c15dd343e
AlexNoyanov <alex.noyanov@gmail.com>
2018-02-26 18:10:47 +0300

    Now all data from serial port can actually converting through the normal numbers (I think so)

--- Servo_Control-_Serial/Servo_Control-_Serial.ino
+++ Servo_Control-_Serial/Servo_Control-_Serial.ino
@@ -8,16 +8,20 @@
 
 #include <Servo.h> // Library for servo
 
-Servo servoX;    // Servo X 
+Servo servoX;   // Servo X 
+Servo servoY;   // Servo Y
 
+char data[32];  // Data array
 
 int angleX;     // X angle
+int angleY;     // Y angle
+
 void setup() {
   // put your setup code here, to run once:
   servoX.attach(8); // Connetcing servo to pin 8
   Serial.begin(9600); // Start serial port on 9600 bod
 
-  Serial.write("=== Ready to read ===");
+  Serial.println("=== Ready to read ===");
   
 }
 
@@ -25,12 +29,27 @@ void loop() {
   // put your main code here, to run repeatedly:
  
   if(Serial.available() > 0){
-     angleX = Serial.read();   // Read X angle
-      
-    Serial.print("X Angle: ");
-    Serial.println(angleX);
-    servoX.write(angleX);     // Rotate servo on angle
     
+   int i = 0;
+
+   // Read all symbols in array
+     for(; Serial.available() > 0; i++){
+      data[i] =  Serial.read();   // Read X angle
+     }
+      
+    // Serial.print("X Angle: ");
+    // Serial.println(angleX);
+    // servoX.write(angleX);     // Rotate servo on angle
+     Serial.println(" ");
+     
+     Serial.print("Inputed data:\n");
+    delay(100);
+     for(int k = 0; k < i; k++){
+      Serial.println(data[k]);
+     }
+
+
+     
     delay(500);               // Wait a litle bit
   }
 }
