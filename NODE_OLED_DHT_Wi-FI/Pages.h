a15d1fb3b423900e1f3ecd08933413d3f83d6b99
AlexNoyanov <alex.noyanov@gmail.com>
2018-07-03 23:30:33 +0300

    ESP8266 moving pages on OLED screen. Showing temperature and air humidity from DHT sensor

--- NODE_OLED_DHT_Wi-FI/Pages.h
+++ NODE_OLED_DHT_Wi-FI/Pages.h
@@ -1,6 +1,11 @@
 #ifndef Pages_h
 #define Pages_h
 
-// All functions to work with moving pages
+
+  // All functions for pages:
+void msOverlay(OLEDDisplay *display, OLEDDisplayUiState* state);  
+void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y)
+
 
 #endif
